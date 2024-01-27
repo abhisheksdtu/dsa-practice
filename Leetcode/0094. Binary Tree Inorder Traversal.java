3827801c9beeabe44a20705ac54f3b665c131b3a
abhishek.singh <abhishek.singh@sirionlabs.com>
2024-01-27 19:25:20 +0530

    leetcode-94

--- Leetcode/0094. Binary Tree Inorder Traversal.java
+++ Leetcode/0094. Binary Tree Inorder Traversal.java
@@ -1,3 +1,72 @@
-public class 0094. Binary Tree Inorder Traversal {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+/*
+ * RECURSIVE
+ * TC - O(N)
+ * SC - O(N)
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        return list;
+    }
+
+    public void inorder(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, list);
+        list.add(node.val);
+        inorder(node.right, list);
+    }
+}
+
+/*
+ * ITERATIVE
+ * TC - O(N)
+ * SC - O(N)
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode node = root;
+
+        while (true) {
+            if (node != null) {
+                st.push(node);
+                node = node.left;
+            } else {
+                if (st.isEmpty()) {
+                    break;
+                }
+                node = st.pop();
+                list.add(node.val);
+                node = node.right;
+            }
+        }
+
+        return list;
+    }
 }
